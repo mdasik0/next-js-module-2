282fd56c08a754ed89cb7904a889a6aef3569de8
mdasik0 <asikthe1st@gmail.com>
2023-10-01 22:09:06 +0600

    next js font, first module complete

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({ subsets: ['latin'], weight: ["100", "300", "400", "500","700"], variable: "--font-roboto" })
 
 export const metadata = {
   title: 'Learning Next Js',
@@ -11,7 +11,7 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${roboto.variable} font-roboto`}>
         {children}
         </body>
     </html>
