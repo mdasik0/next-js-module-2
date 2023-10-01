2c030e1ccef537c74b7ab5765b2df7319cd5b1fa
mdasik0 <asikthe1st@gmail.com>
2023-10-01 21:53:12 +0600

    done

--- src/app/(with-layout)/blogs/[...segements]/page.js
+++ src/app/(with-layout)/blogs/[...segements]/page.js
@@ -3,6 +3,9 @@ import React from 'react';
 const singleBlogs = ({params, searchParams}) => {
 
     const [id, date] = params.segements || [];
+    //NOTE::: you can use searchparams, params hook as well if you are using a client component
+    // const searchParams = useSearchParams.get()
+    // const params = useParams()
 
     return (
         <div className='border border-blue-400 flex flex-col gap-4 container mx-auto p-3 rounded-lg font-semibold'>
