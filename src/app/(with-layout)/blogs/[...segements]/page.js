4d70c2724107845870a56cbea620d4d0e0453200
mdasik0 <asikthe1st@gmail.com>
2023-10-01 21:51:36 +0600

    use router done, use params done, use searchparams done

--- src/app/(with-layout)/blogs/[...segements]/page.js
+++ src/app/(with-layout)/blogs/[...segements]/page.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const singleBlogs = ({params}) => {
+const singleBlogs = ({params, searchParams}) => {
 
-    const [id, title, author, date] = params.segements || [];
+    const [id, date] = params.segements || [];
 
     return (
         <div className='border border-blue-400 flex flex-col gap-4 container mx-auto p-3 rounded-lg font-semibold'>
             <h3>blog id: {id}</h3>
-            <h2 className='text-xl'>Title: {title}</h2>
-            <span className='text-lg'>Author: {author}</span>
+            <h2 className='text-xl'>Title: {searchParams.title}</h2>
+            <span className='text-lg'>Author: {searchParams.author}</span>
             <h6>Date: {date}</h6>
         </div>
     );
--- src/app/(with-layout)/blogs/page.js
+++ src/app/(with-layout)/blogs/page.js
@@ -1,7 +1,10 @@
 import Link from 'next/link';
+// import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const BlogsPage = () => {
+
+  // const router = useRouter()
 
     const blogs = [
         {
@@ -26,7 +29,15 @@ const BlogsPage = () => {
     return (
         <div className='flex flex-col gap-3 container mx-auto'>
             {
-                blogs.map(({id,title,author,date}) => <Link className='border border-blue-400 p-2 rounded text-sm font-semibold' key={id} href={`/blogs/${id}/${title}/${author}/${date}`}>{title}</Link>)
+                blogs.map(({id,title,author,date}) => <Link className='border border-blue-400 p-2 rounded text-sm font-semibold' key={id} 
+                //NOTE::: onclick(() => router.push(/blog/${id}/${date}?title=${title}?author=${author}) --- this works as well if you are using client component
+                href={{
+                  pathname: `/blogs/${id}/${date}`,
+                  query: {
+                    title: title,
+                    author: author,
+                  }
+                }}>{title}</Link>)
             }
         </div>
     );
