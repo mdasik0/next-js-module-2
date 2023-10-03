9456df0689309eff32434bc01e9ed4d240762861
mdasik0 <asikthe1st@gmail.com>
2023-10-03 08:58:22 +0600

    data fetching and ssg

--- src/app/(with-layout)/blogs/[id]/page.js
+++ src/app/(with-layout)/blogs/[id]/page.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
-const singleBlogs = ({params, searchParams}) => {
+const singleBlogs = ({params}) => {
 
-    const [id, date] = params.segements || [];
+    // const [id, date] = params.segements || [];
     //NOTE::: you can use searchparams, params hook as well if you are using a client component
     // const searchParams = useSearchParams.get()
     // const params = useParams()
 
     return (
         <div className='border border-blue-400 flex flex-col gap-4 container mx-auto p-3 rounded-lg font-semibold'>
-            <h3>blog id: {id}</h3>
-            <h2 className='text-xl'>Title: {searchParams.title}</h2>
+            <h3>blog id: {params?.id}</h3>
+            {/* <h2 className='text-xl'>Title: {searchParams.title}</h2>
             <span className='text-lg'>Author: {searchParams.author}</span>
-            <h6>Date: {date}</h6>
+            <h6>Date: {date}</h6> */}
         </div>
     );
 };
--- src/app/(with-layout)/blogs/page.js
+++ src/app/(with-layout)/blogs/page.js
@@ -1,46 +1,53 @@
-import Link from 'next/link';
+import Link from "next/link";
 // import { useRouter } from 'next/navigation';
-import React from 'react';
+import React from "react";
 
-const BlogsPage = () => {
-
+const BlogsPage = async () => {
   // const router = useRouter()
 
-    const blogs = [
-        {
-          id: 1,
-          title: "Getting Started with JavaScript",
-          author: "John Doe",
-          date: 2023
-        },
-        {
-          id: 2,
-          title: "Introduction to ReactJS",
-          author: "Jane Smith",
-          date: 2023
-        },
-        {
-          id: 3,
-          title: "Python Web Development with Django",
-          author: "David Johnson",
-          date: 2022
-        }
-      ];
-    return (
-        <div className='flex flex-col gap-3 container mx-auto'>
-            {
-                blogs.map(({id,title,author,date}) => <Link className='border border-blue-400 p-2 rounded text-sm font-semibold' key={id} 
-                //NOTE::: onclick(() => router.push(/blog/${id}/${date}?title=${title}?author=${author}) --- this works as well if you are using client component
-                href={{
-                  pathname: `/blogs/${id}/${date}`,
-                  query: {
-                    title: title,
-                    author: author,
-                  }
-                }}>{title}</Link>)
-            }
+  // const blogs = [
+  //     {
+  //       id: 1,
+  //       title: "Getting Started with JavaScript",
+  //       author: "John Doe",
+  //       date: 2023
+  //     },
+  //     {
+  //       id: 2,
+  //       title: "Introduction to ReactJS",
+  //       author: "Jane Smith",
+  //       date: 2023
+  //     },
+  //     {
+  //       id: 3,
+  //       title: "Python Web Development with Django",
+  //       author: "David Johnson",
+  //       date: 2022
+  //     }
+  //   ];
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`,{
+    cache: "force-cache"
+  });
+  const blogs = await res.json();
+  return (
+    <div className="flex flex-col gap-3 container mx-auto">
+      {blogs.map(({ id, title, body }) => (
+        <div
+          className="border border-blue-400 p-2 rounded text-sm font-semibold"
+          key={id}
+        >
+          <h4 className="text-2xl">{title}</h4>
+
+          <Link
+            className="text-white mt-6 bg-blue-500 rounded-sm px-3 py-1 inline-block"
+            href={`/blogs/${id}`}
+          >
+            <button>View Details</button>
+          </Link>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default BlogsPage;
