9cb4a2740f23e7907343779078e6e6d997702da9
mdasik0 <asikthe1st@gmail.com>
2023-10-04 21:51:01 +0600

    done

--- src/app/(with-layout)/blogs/[id]/page.js
+++ src/app/(with-layout)/blogs/[id]/page.js
@@ -1,25 +1,31 @@
-import getBlogs from '@/utils/getBlogs';
-import getSingleBlog from '@/utils/getSingleBlog';
-import React from 'react';
+import getSingleBlog from "@/utils/getSingleBlog";
+import React from "react";
 
-const singleBlogs = async ({params}) => {
+export const generateMetadata = async ({ params }) => {
+  const singleBlog = await getSingleBlog(params.id);
+  return {
+    title: singleBlog?.title,
+  };
+};
 
-    // const [id, date] = params.segements || [];
-    //NOTE::: you can use searchparams, params hook as well if you are using a client component
-    // const searchParams = useSearchParams.get()
-    // const params = useParams()
+const singleBlogs = async ({ params }) => {
+  console.log(params.id);
+  // const [id, date] = params.segements || [];
+  //NOTE::: you can use searchparams, params hook as well if you are using a client component
+  // const searchParams = useSearchParams.get()
+  // const params = useParams()
 
-    const singleBlog = await getSingleBlog(params.id)
+  const singleBlog = await getSingleBlog(params.id);
 
-    // const singleBlog = allBlogs.find(d => d.id == params.id)
+  // const singleBlog = allBlogs.find(d => d.id == params.id)
 
-    return (
-        <div className='border border-blue-400 flex flex-col gap-4 container mx-auto p-3 rounded-lg font-semibold'>
-            <h3>blog id: {params?.id}</h3>
-            <h2 className='text-xl'>Title: {singleBlog?.title}</h2>
-            <span className='text-lg'>Author: {singleBlog?.body}</span>
-        </div>
-    );
+  return (
+    <div className="border border-blue-400 flex flex-col gap-4 container mx-auto p-3 rounded-lg font-semibold">
+      <h3>blog id: {params?.id}</h3>
+      <h2 className="text-xl">Title: {singleBlog?.title}</h2>
+      <span className="text-lg">Author: {singleBlog?.body}</span>
+    </div>
+  );
 };
 
 export default singleBlogs;
--- src/app/(with-layout)/blogs/page.js
+++ src/app/(with-layout)/blogs/page.js
@@ -3,29 +3,13 @@ import Link from "next/link";
 // import { useRouter } from 'next/navigation';
 import React from "react";
 
-const BlogsPage = async () => {
-  // const router = useRouter()
+export const metadata =  {
+  title: "blogs | Next Js.",
+  description: "this is a next js app"
+}
 
-  // const blogs = [
-  //     {
-  //       id: 1,
-  //       title: "Getting Started with JavaScript",
-  //       author: "John Doe",
-  //       date: 2023
-  //     },
-  //     {
-  //       id: 2,
-  //       title: "Introduction to ReactJS",
-  //       author: "Jane Smith",
-  //       date: 2023
-  //     },
-  //     {
-  //       id: 3,
-  //       title: "Python Web Development with Django",
-  //       author: "David Johnson",
-  //       date: 2022
-  //     }
-  //   ];
+const BlogsPage = async () => {
+ 
   const blogs = await getBlogs()
   return (
     <div className="flex flex-col gap-3 container mx-auto">
