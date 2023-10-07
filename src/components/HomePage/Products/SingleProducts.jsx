bb94f9b48c712a03819b74cdbdf92e2c08cf908a
mdasik0 <asikthe1st@gmail.com>
2023-10-07 19:39:39 +0600

    done

--- src/components/HomePage/Products/SingleProducts.jsx
+++ src/components/HomePage/Products/SingleProducts.jsx
@@ -6,6 +6,7 @@ const SingleProducts = ({product}) => {
     const {id, title, price} = product;
     return (
         <div className='border border-red-500 shadow-lg  p-2 rounded-lg'>
+            
             <Link className='cursor-pointer' href={`/products/${id}`}>{title}</Link>
         </div>
     );
