ea7c68374e379d16ba4d4885ea89139eb1742525
mdasik0 <asikthe1st@gmail.com>
2023-10-08 23:50:30 +0600

    just keeping the streak

--- src/components/HomePage/Products/SingleProducts.jsx
+++ src/components/HomePage/Products/SingleProducts.jsx
@@ -6,7 +6,6 @@ const SingleProducts = ({product}) => {
     const {id, title, price} = product;
     return (
         <div className='border border-red-500 shadow-lg  p-2 rounded-lg'>
-            
             <Link className='cursor-pointer' href={`/products/${id}`}>{title}</Link>
         </div>
     );
